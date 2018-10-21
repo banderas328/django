467101418570b1796da2330172ab1ac0e61d9c08
Anton Zhauryd <banderas328@yandex.ru>
2018-10-21 22:26:15 +0300

    some staff

--- fullproject/blog/models.py
+++ fullproject/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=50)
+    body = models.TextField(max_length=400)
+    pubdate = models.DateField()
+
+
